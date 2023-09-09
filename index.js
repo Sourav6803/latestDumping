73566a61c6da8a4c3eef4d9ad4d42b4e36c8220c
Sourav6803 <106535807+Sourav6803@users.noreply.github.com>
2023-09-09 19:14:08 +0530

    Update index.js
    dkjbd

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use( multer().any())
 
 app.get('/', (req,res)=>{
-    res.send("Welcome to Dumpong Data  ")
+    res.send("Welcome to Dumpong Data dd ")
 })
 
 
