167a1b7d22ef2017766182e1270bae5b6afe1efc
Sourav6803 <106535807+Sourav6803@users.noreply.github.com>
2023-09-09 19:01:13 +0530

    Update index.js
    ghgghd

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use( multer().any())
 
 app.get('/', (req,res)=>{
-    res.send("Welcome to Dumpong Data super awsome ")
+    res.send("Welcome to Dumpong Data  ")
 })
 
 
